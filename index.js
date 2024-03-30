c6e96bec2fece6b60bb1569696c651a4aabfb336
SOFIA-ASIF <160203172+SOFIA-ASIF@users.noreply.github.com>
2024-03-30 00:48:08 -0700

    make basic todo list app

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todolist = await inquirer.prompt([
+        {
+            name: "list",
+            message: "Add your task",
+            type: "input"
+        },
+        {
+            name: "addmore",
+            message: "Do you want to add more",
+            type: "confirm",
+            default: false
+        }
+    ]);
+    todos.push(todolist.list);
+    condition = todolist.addmore;
+}
+console.log(todos);
